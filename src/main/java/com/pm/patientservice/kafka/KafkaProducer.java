51add164c60a3087a0b96a70b313de3d33cd6705
DilshanJay <sdjayawardana18@gmail.com>
2026-07-04 20:22:18 +0530

    feat: implement Kafka producer for patient events and add configuration properties

--- src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,13 +1,37 @@
 package com.pm.patientservice.kafka;
 
+import com.pm.patientservice.enums.PatientEventType;
+import com.pm.patientservice.model.Patient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
+@Slf4j
 @Service
 public class KafkaProducer {
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
+    @Value("${spring.kafka.topic.patient}")
+    private String patientTopic;
+
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType(PatientEventType.PATIENT_CREATED.name())
+                .build();
+
+        try {
+            kafkaTemplate.send(patientTopic, event.toByteArray());
+        } catch (Exception e) {
+            log.error("Failed to send event to Kafka Topic: {} Event {}", e.getMessage(), event);
+        }
     }
 }
